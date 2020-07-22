349ecccd48de93a2d59c53e18e64ebdeb132e642
Elias Sjögreen <eliassjogreen1@gmail.com>
2020-07-22 17:14:59 +0200

    feat: Try to convert pat to file url
    This allows for fetching local files without adding the file prefix

--- helpers.ts
+++ helpers.ts
@@ -7,10 +7,24 @@ export function protocol(protocol: string) {
 export function toURL(url: string | URL): URL {
   if (typeof url === "string") {
     try {
-      url = new URL(url);
+      try {
+        url = new URL(url);
+      } catch {
+        url = toFileUrl(url as string);
+      }
     } catch (error) {
       throw new CacheError(error.message);
     }
   }
   return url;
+}
+
+export function toFileUrl(url: string): URL {
+    let pathName = url.replace(/\\/g, '/');
+
+    if (pathName[0] !== '/') {
+        pathName = '/' + pathName;
+    }
+
+    return new URL(encodeURI(`file://${pathName}`).replace(/[?#]/g, encodeURIComponent));
 }
