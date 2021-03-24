a87473858905e7e7d0a3ebba532407b36757102a
Luca Casonato <lucacasonato@yahoo.com>
2021-03-24 19:32:57 +0100

    chore: support deno stable

--- deps.ts
+++ deps.ts
@@ -7,10 +7,7 @@ export {
   toFileUrl,
 } from "https://deno.land/std@0.91.0/path/mod.ts";
 
-export {
-  ensureDir,
-  exists,
-  existsSync,
-} from "https://deno.land/std@0.91.0/fs/mod.ts";
+export { ensureDir } from "https://deno.land/std@0.91.0/fs/ensure_dir.ts";
+export { exists, existsSync } from "https://deno.land/std@0.91.0/fs/exists.ts";
 
 export { createHash } from "https://deno.land/std@0.91.0/hash/mod.ts";
